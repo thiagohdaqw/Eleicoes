dab5628de3db92e1b9479a5c262f4f4e56ee09b8
RcleydsonR <rafael.cleydson@gmail.com>
2022-09-18 18:47:46 -0300

    Pequenos ajustes geração de palavras e nomes de tópicos
    Co-authored-by: Thiago Paiva <thiagohdaqw@gmail.com>
    Co-authored-by: Eduardo Afonso <oeduardoafonso@gmail.com>

--- twitter_generator.py
+++ twitter_generator.py
@@ -10,9 +10,10 @@ TWITTER_BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
 TWITTER_INTERVAL_SECONDS = os.getenv('TWITTER_INTERVAL_SECONDS', 60)
 KAFKA_SERVER = os.getenv("KAFKA_SERVER", '0.0.0.0:9092')
 WORDS_TOPIC = os.getenv("WORDS_TOPIC", "wc")
+ELECTION_TOPIC = os.getenv("ELECTION_TOPIC", "election")
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-client = tweepy.Client(bearer_token=TWITTER_BEARER_TOKEN)
+twitter_client = tweepy.Client(bearer_token=TWITTER_BEARER_TOKEN)
 
 def on_send_success(record_metadata):
     print(record_metadata.topic)
@@ -24,15 +25,24 @@ def on_send_error(excp):
 candidates = ['Lula', 'Bolsonaro', 'Simone Tebet', 'Ciro Gomes']
 
 while True:
+    # send to word count topic
+    tweets = twitter_client.search_recent_tweets(query="covid", max_results=100)
+
+    sentences = [tweet.text for tweet in tweets.data]
+
+    for sentence in sentences:
+        producer.send(WORDS_TOPIC, sentence.encode()).add_callback(on_send_success).add_errback(on_send_error)
+        producer.flush()
+
     # send to candidate topic
     for candidate in candidates:
-        tweets = client.search_recent_tweets(query=candidate, max_results=10)
+        candidate_tweets = twitter_client.search_recent_tweets(query=candidate, max_results=10)
 
-        sentences = [tweet.text for tweet in tweets.data]
+        sentences = [tweet.text for tweet in candidate_tweets.data]
 
         for sentence in sentences:
             phrase = f'${candidate},${sentence}'
-            producer.send(WORDS_TOPIC, phrase.encode()).add_callback(on_send_success).add_errback(on_send_error)
+            producer.send(ELECTION_TOPIC, phrase.encode()).add_callback(on_send_success).add_errback(on_send_error)
             producer.flush()
 
     print(f"Sleeping for {TWITTER_INTERVAL_SECONDS} seconds")
