c9b46f13f66909ad05dbda7eb6f85022c35cbe9f
Thiago Paiva <thiagohdaqw@gmail.com>
2022-09-18 14:46:43 -0300

    Removendo acento e genero do dataset

--- clean.py
+++ clean.py
@@ -1,13 +1,20 @@
 import re
+from unidecode import unidecode
 
 STOPWORDS_PATH = "dataset/stopwords.txt"
-CLEAN_REGEX = r"[.,/\\\[\]\{\}`~^\d&!@#$%*\)\(\'\"<>=+-:;?]"
+CLEAN_REGEX = r"[.,/\\\[\]\{\}`~^\d&!@#$%*\)\(\'\"<>=+-:;?“]"
 
 stopwords = set()
 
 with open(STOPWORDS_PATH, "r") as stop_file:
     for w in stop_file:
         stopwords.add(w.strip().lower())
+
+def remove_genre(word):
+    return " ".join(re.sub(r"[oa]s?$", "", w) for w in word.split())
+
+def remove_accent(word):
+    return unidecode(word)
 
 def cleaner(sentence):
     sentence = " ".join(
--- cleaner_dataset.py
+++ cleaner_dataset.py
@@ -1,4 +1,5 @@
-from clean import cleaner
+from clean import cleaner, remove_accent, remove_genre
+from unidecode import unidecode
 
 POSITIVE_PATH = "dataset/positivas.txt"
 NEGATIVE_PATH = "dataset/negativas.txt"
@@ -15,7 +16,12 @@ negatives = set()
 def read_file(path, words):
     with open(path, "r") as file:
         for word in file:
-            words.add(cleaner(word))
+            word = cleaner(word)
+            accentless_word = unidecode(word)
+            words.add(word)
+            words.add(accentless_word)
+            words.add(remove_genre(word))
+            words.add(remove_genre(accentless_word))
 
 def write_dataset(file, words, number):
     words.remove("")
@@ -27,5 +33,6 @@ read_file(POSITIVE_PATH, positives)
 read_file(NEGATIVE_PATH, negatives)
 
 with open(DATASET_PATH, "w") as dataset_file:
+    dataset_file.write(f"sentiment{DELIMITER}sentence\n")
     write_dataset(dataset_file, positives, POSITIVE_NUMBER)
     write_dataset(dataset_file, negatives, NEGATIVE_NUMBER)
